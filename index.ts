6bcac271aa58933c60dc53b35cf0416bd4e1cfa5
coderx4343 <kannangk4343@gmail.com>
2022-08-18 23:12:07 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -8,14 +8,25 @@ class PrinterClass {
   doPrint():void {
      console.log("doPrint() from Parent called…") 
   } 
+  dosum():void{
+    console.log(1*2);
+  }
 } 
 
 class StringPrinter extends PrinterClass { 
-  doPrint():void { 
+  doPrint():void { //redefining the parent class method is known as overriding
      super.doPrint() 
      console.log("doPrint() is printing a string…")
   } 
 } 
+class Sumup extends PrinterClass{
+  dosum():void{
+    super.dosum()
+    console.log(1000/10);
+  }
+}
 
 var obj = new StringPrinter() 
 obj.doPrint()
+var obj2=new Sumup()
+obj2.dosum();
